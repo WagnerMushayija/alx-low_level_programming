2d5b8aa798474aa24bd60f784e552ee10284fe15
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-05 12:13:07 -0700

    ld

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - explanation
+ * Return: 0 pro
+ **/
+int main(void)
+{
+	/*short description for main programm*/
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
