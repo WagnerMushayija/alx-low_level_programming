56cc93b12c0f37cfb0f69d42a2d2976416704bc6
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-05 11:53:18 -0700

    new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -entry point of the preogram
+ * Return: 0 wagner
+ **/
+int main(void)
+{
+	/*short description for main*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
