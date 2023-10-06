a0a0761d5871014f08676ebebc463599af1ea302
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-06 08:45:40 -0700

    new

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - explanations
+ * Return: 0 expected
+ **/
+int main(void)
+{
+	/*small explaination of main*/
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %d byte(s)\n", sizeof(long long));
+	printf("Size of a float: %d bytse(s)\n", sizeof(float));
+	return (0);
+}
