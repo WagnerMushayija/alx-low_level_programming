aa2787816fc893f29d2390625f20e975ec592eb4
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-10 12:12:00 -0700

    completed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
-#include<unistd.h>
-#include<main.h>
+#include "main.h"
+
 /**
- * main - printing _putchar w/o printf
- * Return: (0) Successful
- **/
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	/*tha main function*/
-	write(2, "_putchar\n", 8);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
