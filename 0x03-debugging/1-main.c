412e3be9b407942af02ddaa928192bf83e7d66f6
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-11 10:22:44 -0700

    new

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,9 +13,9 @@ int main(void)
 
         i = 0;
 
-        while (i < 10)
+        while (i < 10)//infinite loop starts here
         {
-                putchar(i);//you had to increment
+                putchar(i);
         }
 
         printf("Infinite loop avoided! \\o/\n");
