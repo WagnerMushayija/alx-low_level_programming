df9887ad2f2b552c5c837ecc4565d344217f8cb0
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-11 09:47:06 -0700

    shit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,19 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
+void positive_or_negative(int i)
+{
+	int i;
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	return (0);
+}
 
 #endif /* MAIN_H */
