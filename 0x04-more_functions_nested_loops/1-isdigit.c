2493925c40c9afb8b1540cb7fae8cb1081485c8c
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-12 12:51:06 -0700

    new

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,16 +2,16 @@
 /**
  * _isdigit - checks for a digit
  * Return: 1 or 0 if sucess
+ * @c: the variable being checked
  */
-
 int _isdigit(int c)
 {
-    if (c >= '0' && c <= '9')
-    {
-        return (1); 
-    }
-    else
-    {
-        return (0);
-    }
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
