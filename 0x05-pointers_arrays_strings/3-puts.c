5b5d7f4cffe1cafdfc325ac89616c640c06c1986
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-16 11:40:32 -0700

    new

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints out with new line
+ * @str: string used
+ * Return: 0 if successful
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
