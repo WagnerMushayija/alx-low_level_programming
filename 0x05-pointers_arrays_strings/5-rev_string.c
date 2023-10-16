290328c78be0699031da761f3b7e09afc0e42721
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-16 12:13:48 -0700

    nevr

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - reversing strings entered
+ * @s:string to be printed
+ * Return: 0 if successful
+ */
+void rev_string(char *s)
+{
+	int len = 0, start, end;
+
+	char temp;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	satrt = 0;
+	end = len - 1;
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+
+		start++;
+		end--;
+	}
+}
