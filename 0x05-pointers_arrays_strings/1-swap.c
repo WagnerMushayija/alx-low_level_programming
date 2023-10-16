84fa77f941985ea614cffdc6b0d4094f91103c53
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-16 10:48:22 -0700

    new

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * swap_int - swap 2 numbers
+ * @a: first number
+ * @b: second number
+ * Return: 0 if success
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
