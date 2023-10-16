cd90e715e0ba8f8e3ae85783ba87d9463025abd1
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-16 11:13:15 -0700

    new

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _strlen - gives the length
+ * @s: variable to be used
+ * Return: 0 if successful
+ */
+int _strlen(int *s)
+{
+	int len;
+
+	len = strlen(s)
+	return (0);
+}
