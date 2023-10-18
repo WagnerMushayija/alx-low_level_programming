a1abc0e906fbbabf24177b475e5abe728686426f
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-18 10:34:20 -0700

    new

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,4 +14,18 @@ char *_strcat(char *dest, char *src)
 
 	int i = 0;
 
-	while (dest_len)
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	while (src[src_len] != '\0')
+	{
+		src_len++;
+	}
+	for (i = 0; i < src_len; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + src_len] = '\0';
+	return (dest);
+}
