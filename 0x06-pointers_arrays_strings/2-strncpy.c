5cf0d79d2a5f5ff4426fa76eba802ff701e4836e
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-19 10:34:42 -0700

    new

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,15 +3,16 @@
 /**
  * _strncpy - prototype to be used
  * @dest: string used
- * @src: string used 
+ * @src: string used
  * @n: number counted
  * return: 0 if sucess
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while(i < n && src[i] != '\0')
+	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
