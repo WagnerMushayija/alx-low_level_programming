8f2ffa77e50f59b159c260595f706c9888d50aa4
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-19 09:22:29 -0700

    new

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,20 +4,19 @@
  * _strncat - function to be executed
  * @dest: first string
  * @src: second string
- * @n: it's size 
+ * @n: it's size
  * Return: 0 if successful
  */
 char _strncat(char *dest, char *src, int n)
 {
 	int dest_len = 0;
 
-	int i;
+	int i = 0;
 
 	while (dest[dest_len] != '\0')
 	{
 		dest_len++;
 	}
-	i = 0;
 	while (src[i] != '\0' && i < n)
 	{
 		dest[dest_len + i] = src[i];
