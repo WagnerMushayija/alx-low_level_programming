d220f54461a9e8e750300db1f6ff0df5941c1bc7
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-23 11:21:14 -0700

    new

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string used
+ * @accept: pointer of the set of characters
+ * Return: idk
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+
+	int found;
+
+	while (*s)
+	{
+		found = 0;
+		
+		for (int i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				found = 1;
+				break;
+			}
+		}
+
+	if (found)
+	{
+		count++;
+	}
+	else
+	{
+		break;
+	}
+	s++;
+	}
+	return (count);
+}
