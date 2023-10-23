f710c1455e56c1a6ceca7d3e935f5be997bb3928
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-23 11:42:39 -0700

    new

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strbrk - searches for a string for any set of bytes
+ * @s: string to be used
+ * @accept: pointer to the string
+ * Return: nothing
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		char *ch = accept;
+
+		while (*ch != '\0')
+		{
+			if (*s == *ch)
+			{
+				return (s);
+			}
+			ch++;
+		}
+		s++;
+	}
+	return (NULL);
+}
