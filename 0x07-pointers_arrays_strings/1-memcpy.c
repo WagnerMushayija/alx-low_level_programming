b268f1b025bfe5fb2f4019ec10f8a0b8539d9939
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-23 10:38:40 -0700

    hope

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memcpy - copy data from a to be
+ * @dest: the pointer destination
+ * @src: the pointer place it's at
+ * @n: how much u wanna copy
+ * Return: pointer
+ */
+cahr *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0, i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
