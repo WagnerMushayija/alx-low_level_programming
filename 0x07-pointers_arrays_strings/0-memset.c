e967e690296fefbf5e7fc44b6f291af92a3cdc32
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-23 10:20:32 -0700

    new

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _mesmet - function to keep a constant
+ * _memset - function to keep a constant
  * @s: the variable
  * @b: the character
  * @n: the size you want to fill
@@ -11,7 +11,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++) 
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
