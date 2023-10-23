02f0362ddc80c4d2b3dd18a10180622b3d72fcc6
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-23 12:04:10 -0700

    new

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_chessboard - printing chessboard
+ * @a: two dimensions array
+ * Return: idk
+ */
+void print_chessboard(char (*a[8]))
+{
+	int i, j;
+
+	for (i = 0; j < 8; j++)
+	{
+		for (j =0; < 8; j++)
+		{
+			return (a[i][j]);
+		}
+		return ('\n');
+	}
+}
