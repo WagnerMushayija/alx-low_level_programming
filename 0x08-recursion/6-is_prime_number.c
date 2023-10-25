a0d3a2a7b4ee52867c21edb72114e189baa2c0c2
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-25 12:07:33 -0700

    istg

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * is_prime_number - checking if its prime or not
+ * @n: number entered
+ * Return: watevs
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (act_prime(n, n - 1));
+}
+
+
+/**
+ * act_prime - calculates recursively
+ * @n: numner to be looked
+ * @m:ayterator
+ * Return: 0watevs
+ */
+int act_prime(int n, int m)
+{
+	if (n % m == 0 && m > 0)
+	{
+		return (0);
+	}
+	if (m == 0)
+	{
+		return (1);
+	}
+	return (act_prime(n, m - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,4 +9,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int act_sqrt_rec(int n, int m);
+int is_prime_number(int n);
+int act_prime(int n, int m);
 #endif
