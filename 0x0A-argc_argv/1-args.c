8b1ec97cbde91ad04099c427aae4addbc73d791a
WagnerMushayija <gigiwagner32@gmail.com>
2023-10-27 10:33:49 -0700

    new

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - return string
+ * @argc: arguement count
+ * @argv: argument vector
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
