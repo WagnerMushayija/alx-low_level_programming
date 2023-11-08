a49a28f05f4f1625c885c8acf18ab3db87b59497
WagnerMushayija <gigiwagner32@gmail.com>
2023-11-08 11:00:43 -0800

    wat

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,9 +3,14 @@
 /**
  * print_name - function to a pointer printing a name
  * @name; name pointer
+ * @f: pointer address
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (!name || !f)
+	{
+		return;
+	}
 	f(name);
 }
